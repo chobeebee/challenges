17e0d1dcfa87ea6c2a34fec90e91b53bdf54e416
cho <websera12@gmail.com>
2025-05-12 23:42:20 +0900

    git init

---
+++ src/test/java/com/baro/challenges/ChallengesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baro.challenges;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
